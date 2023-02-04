d10590a9e871b8335d4d094bd56758d7f2a59387
mczupryn1 <81737272+mczupryn1@users.noreply.github.com>
2023-02-04 17:49:16 -0500

    added javascript

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Your code here
+var trueOrFalseString = 'True or False: '
+
+var questionsArr = [
+    { question: trueOrFalseString + "German chocolate cake was invented in Texas", 
+    answer: true },
+    { question: trueOrFalseString + 'Dead skin cells are a main ingredient in household dust.', 
+    answer: true },
+    { question: trueOrFalseString + 'When a bat leaves a cave, it turns right.', 
+    answer: false },
+    { question: trueOrFalseString + 'Babies and adults have the same number of bones', 
+    answer: false },
+    { question: trueOrFalseString + 'The mythical unicorn is the national animal of Scotland.', 
+    answer: true }
+]
+
+console.log(questionsArr);
+
+function runQuiz() {
+    var score = 0
+    for (var i = 0; i < questionsArr.length; i++) {
+        var userAnswer = confirm(questionsArr[i].question);
+        if (userAnswer == questionsArr[i].answer) 
+        {score++;}
+    }
+    var percent = (score / questionsArr.length) * 100;
+    var roundedPercent = Math.round(percent);
+    alert("You scored a " + roundedPercent + "%!");
+}
